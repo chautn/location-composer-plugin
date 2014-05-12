c4ce433f2479a8cbd2fd68c709ac4b4c22c0aa3b
Chau Tran <chautn@exoplatform.com>
2014-05-12 11:06:37 +0700

    Init project

---
+++ composer-plugin/src/main/java/com/acme/samples/SampleActivityComposer.java
@@ -0,0 +1,144 @@
+package com.acme.samples;
+
+import java.util.LinkedHashMap;
+
+import org.exoplatform.social.webui.composer.UIActivityComposer;
+import org.exoplatform.social.webui.composer.UIComposer.PostContext;
+import org.exoplatform.webui.application.WebuiRequestContext;
+import org.exoplatform.webui.core.UIComponent;
+import org.exoplatform.webui.event.Event;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.exoplatform.social.core.activity.model.ExoSocialActivity;
+import org.exoplatform.social.core.activity.model.ExoSocialActivityImpl;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.identity.provider.SpaceIdentityProvider;
+import org.exoplatform.social.core.manager.ActivityManager;
+import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.social.core.space.model.Space;
+import org.exoplatform.social.webui.activity.UIActivitiesContainer;
+import org.exoplatform.social.webui.composer.UIComposer;
+import org.exoplatform.social.webui.profile.UIUserActivitiesDisplay;
+import org.exoplatform.social.webui.space.UISpaceActivitiesDisplay;
+import org.exoplatform.web.application.ApplicationMessage;
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.UIApplication;
+import org.exoplatform.webui.event.EventListener;
+import org.exoplatform.webui.form.UIFormStringInput;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+@ComponentConfig(template = "classpath:groovy/com/acme/samples/SampleActivityComposer.gtmpl", events = {
+        @EventConfig(listeners = SampleActivityComposer.CheckinActionListener.class),
+        @EventConfig(listeners = UIActivityComposer.CloseActionListener.class),
+        @EventConfig(listeners = UIActivityComposer.SubmitContentActionListener.class),
+        @EventConfig(listeners = UIActivityComposer.ActivateActionListener.class) })
+public class SampleActivityComposer extends UIActivityComposer {
+    
+    public static final String LOCATION = "location";
+    private String location_;
+    private boolean isLocationValid_ = false;
+    private Map<String, String> templateParams;
+    
+    
+    public SampleActivityComposer() {
+        System.out.println(SampleActivityComposer.class + ": " + "constructor called!");
+        //WebuiRequestContext requestContext = WebuiRequestContext.getCurrentInstance();
+        //ResourceBundle resourceBundle = requestContext.getApplicationResourceBundle();     
+        System.out.println("isReady= " + isReadyForPostingActivity()); 
+        System.out.println("isDisplayed= " + isDisplayed());
+        setReadyForPostingActivity(false);
+        UIFormStringInput inputLocation = new UIFormStringInput("InputLocation", "InputLocation", null);
+        addChild(inputLocation);
+        setReadyForPostingActivity(true);
+        setDisplayed(true);
+        System.out.println("isReady= " + isReadyForPostingActivity());
+        System.out.println("isDisplayed= " + isDisplayed());
+    }
+    
+    public void setLocationValid(boolean isValid) {
+        isLocationValid_ = isValid;
+    }
+    
+    public boolean isLocationValid() {
+        return isLocationValid_;
+    }
+    
+    public void setTemplateParams(Map<String, String> tempParams) {
+        templateParams = tempParams;
+    }
+    
+    public Map<String, String> getTemplateParams() {
+        return templateParams;
+    }
+    
+    public void clearLocation() {
+        location_ = null;
+    }
+    
+    public String getLocation() {
+        return location_;
+    }
+    
+    private void setLocation(String city, WebuiRequestContext requestContext) {
+        location_ = city;
+        if (location_ == null || location_ == "") {
+            UIApplication uiApp = requestContext.getUIApplication();
+            uiApp.addMessage(new ApplicationMessage("abc", null, ApplicationMessage.WARNING));
+            return;
+        }
+        
+        templateParams = new LinkedHashMap<String, String>();
+        templateParams.put(LOCATION, location_);
+        
+        setLocationValid(true);
+    }
+    
+    @Override
+    public void onActivate(Event<UIActivityComposer> uiActivityComposer) {}
+    
+    @Override
+    public void onSubmit(Event<UIActivityComposer> uiActivityComposer) {}
+    
+    @Override
+    public void onClose(Event<UIActivityComposer> uiActivityComposer) {}
+    
+    @Override
+    public void onPostActivity(PostContext postContext, UIComponent uiComponent, WebuiRequestContext webuiRequestContext, String str) {}
+    
+    public static class CheckinActionListener extends EventListener<SampleActivityComposer> {
+        @Override
+        public void execute(Event<SampleActivityComposer> event) throws Exception {
+            
+            System.out.println("Checkin button was clicked!");
+            WebuiRequestContext requestContext = event.getRequestContext();
+            SampleActivityComposer sampleActivityComposer = event.getSource();
+            UIFormStringInput form_ = sampleActivityComposer.getChildById("InputLocation");
+            String city_val = form_.getValue();
+            System.out.println("city_val= " + city_val);
+            //sampleActivityComposer.setReadyForPostingActivity(true);
+            
+            String city;
+            try {
+                city = requestContext.getRequestParameter(OBJECTID).trim();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+            
+            System.out.println("City= " + city);
+            
+            sampleActivityComposer.setLocation(city, requestContext);       
+            System.out.println(CheckinActionListener.class + ": " + sampleActivityComposer.location_);
+            if (sampleActivityComposer.location_.length() > 0) {
+                requestContext.addUIComponentToUpdateByAjax(sampleActivityComposer);
+                event.getSource().setReadyForPostingActivity(true);
+            }
+        }
+    }
+
+}
---
+++ resources/src/main/webapp/javascript/acme/samples/LocationComposer.js
@@ -0,0 +1,67 @@
+(function($) {
+    var LocationComposer = {
+        ENTER_KEY_CODE: 13,
+        
+        sayHello: function() {
+            alert("Hello");
+        },
+        
+        onLoad: function(params) {
+            LocationComposer.configure(params);
+            LocationComposer.init();
+        },
+        
+        configure: function(params) {
+            this.locationValid = params.locationValid || false;
+            this.inputLocationId = params.inputLocationId || 'inputLocation';
+            this.checkinButtonId = params.checkinButtonId || 'checkinButton';
+            this.checkinUrl = decodeURI(params.checkinUrl || "");
+            this.location = params.location || '';
+        },
+        
+        init: function() {
+            LocationComposer = this;
+            
+            if (this.locationValid) {
+                //
+            } else {
+                this.inputLocation = $('#' + this.inputLocationId);
+                this.checkinButton = $('#' + this.checkinButtonId);
+                
+                var LocationComposer = this;
+                var inputLocation = this.inputLocation;
+                var checkinBtn = this.checkinButton;
+                inputLocation.on('focus', function(evt) {
+                    if (inputLocation.val() === '') {
+                        inputLocation.val('');
+                    }
+                });
+                this.inputLocation.on('keypress', function(evt) {
+                   if (LocationComposer.ENTER_KEY_CODE == (evt.which ? evt.which : evt.keyCode)) {
+                       $(checkinBtn).click();
+                   } 
+                });
+                this.checkinButton.removeAttr('disabled');
+                this.checkinButton.on('click', function(evt) {
+                    if (inputLocation.val() === '') {
+                        return;
+                    }
+                    var url = LocationComposer.checkinUrl.replace(/&amp;/g, "&") + '&objectId=' + inputLocation.val() + '&ajaxRequest=true';
+                    ajaxGet(url, function() {
+                        try {
+                            $('textarea#composerInput').eXoMentions('showButton', function() {});
+                        } catch (e) {}
+                    });
+                });
+            }
+            
+            var closeButton = $('#UIActivityComposerContainer').find('a.uiIconClose:first');
+            if (closeButton.length > 0) {
+                closeButton.on('click', function() {
+                    $('textarea#composerInput').exoMentions('clearLink', function() { });
+                });
+            }
+        }
+    };
+    return LocationComposer;
+})($);
